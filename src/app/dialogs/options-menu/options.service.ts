a0c9abbb92a4d43f5c0f0bfe57f4ac468eccc122
nop990 <62253708+nop990@users.noreply.github.com>
2025-03-26 22:51:52 -0400

    changed ports to 8080

--- src/app/dialogs/options-menu/options.service.ts
+++ src/app/dialogs/options-menu/options.service.ts
@@ -8,52 +8,54 @@ import {OptionsModel} from '../../models/options.model';
 import {optionsForm} from '../../utils';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class OptionsService {
-  private http = inject(HttpClient);
-  private snackbar = inject(MatSnackBar);
+    private http = inject(HttpClient);
+    private snackbar = inject(MatSnackBar);
 
-  private GetOptionsSubject = new BehaviorSubject<void>(undefined);
-  GetOptionsAction = this.GetOptionsSubject.asObservable();
+    private GetOptionsSubject = new BehaviorSubject<void>(undefined);
+    GetOptionsAction = this.GetOptionsSubject.asObservable();
 
-  public sendGetOptions() {
-    this.GetOptionsSubject.next();
-  }
+    public sendGetOptions() {
+        this.GetOptionsSubject.next();
+    }
 
-  getOptions$ = this.GetOptionsAction.pipe(
-    switchMap(() => {
-      return this.http.get('http://localhost:5000/getSettings', {responseType: "text"}).pipe(
-        tap(res => {
-          let response = toml.parse(res).Settings as OptionsModel;
-          console.log(response);
-          optionsForm.patchValue(response);
-          return response;
-        }),
-        catchError((error) => {
-          console.log(error);
-          if (error.status != 404) {
-            this.snackbar.open(`Error: ${error.status}`, 'Dismiss');
-          }
-          return of(error);
+    getOptions$ = this.GetOptionsAction.pipe(
+        switchMap(() => {
+            return this.http.get('http://localhost:8080/getSettings', {responseType: "text"}).pipe(
+                tap(res => {
+                    console.log(res);
+                    let response = toml.parse(res) as OptionsModel;
+                    console.log(response);
+                    optionsForm.patchValue(response);
+                    return response;
+                }),
+                catchError((error) => {
+                    console.log(error);
+                    if (error.status != 404) {
+                        this.snackbar.open(`Error: ${error.status}`, 'Dismiss');
+                    }
+                    return of(error);
+                })
+            );
         })
-      );
-    }));
-
-  public getOptionsSignal = toSignal(this.getOptions$);
-
-  setSettings(form: any) {
-    return this.http.post('http://localhost:5000/setSettings', form.getRawValue()).pipe(
-      tap((res: any) => {
-        this.snackbar.open(res, 'Dismiss', {
-          duration: 7500
-        });
-      }),
-      catchError((error) => {
-        this.snackbar.open(`Error: ${error}`, 'Dismiss');
-        return of(error);
-      })
     );
-  }
+
+    public getOptionsSignal = toSignal(this.getOptions$);
+
+    setSettings(form: any) {
+        return this.http.post('http://localhost:8080/setSettings', form.getRawValue(), {responseType: "text"}).pipe(
+            tap((res: any) => {
+                this.snackbar.open(res, 'Dismiss', {
+                    duration: 7500
+                });
+            }),
+            catchError((error) => {
+                this.snackbar.open(`Error: ${error}`, 'Dismiss');
+                return of(error);
+            })
+        );
+    }
 
 }
