70f1e5ad0f95beca785db727760df805d5329725
nop990 <62253708+nop990@users.noreply.github.com>
2025-03-26 22:52:37 -0400

    added pitcher-specific filter form

--- src/app/pistachio.service.ts
+++ src/app/pistachio.service.ts
@@ -4,92 +4,106 @@ import {MatSnackBar} from "@angular/material/snack-bar";
 import {BehaviorSubject, catchError, filter, of, switchMap, tap} from 'rxjs';
 import {toSignal} from '@angular/core/rxjs-interop';
 import {DataManipulationService} from './data-manipulation.service';
-import {filterForm} from "./utils";
+import {filterFormBatters, filterFormPitchers} from "./utils";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class PistachioService {
-  private http = inject(HttpClient);
-  private snackBar = inject(MatSnackBar);
-  loadingSignal: WritableSignal<boolean> = signal(false);
+    private http = inject(HttpClient);
+    private snackBar = inject(MatSnackBar);
+    loadingSignal: WritableSignal<boolean> = signal(false);
 
-  private dataManipulationService = inject(DataManipulationService);
+    private dataManipulationService = inject(DataManipulationService);
 
-  private getBatterReportSubject = new BehaviorSubject<void>(undefined);
-  getBatterReportAction = this.getBatterReportSubject.asObservable();
+    private getBatterReportSubject = new BehaviorSubject<void>(undefined);
+    getBatterReportAction = this.getBatterReportSubject.asObservable();
 
-  private getPitcherReportSubject = new BehaviorSubject<void>(undefined);
-  getPitcherReportAction = this.getPitcherReportSubject.asObservable();
+    private getPitcherReportSubject = new BehaviorSubject<void>(undefined);
+    getPitcherReportAction = this.getPitcherReportSubject.asObservable();
 
-  public runNotebook() {
-    this.loadingSignal.set(true);
-    filterForm.reset({'search': '', 'filterColumn': 'all', 'flagged': false, 'opsPlusPotentialHasPosition': false, 'majorLeagueOnly': false});
-
-    this.http.post('http://localhost:5000/runNotebook', {}).pipe(
-      tap((res: any) => {
-        this.loadingSignal.set(false);
-        this.snackBar.open(res, 'Dismiss', {
-          duration: 7500
+    public runNotebook() {
+        this.loadingSignal.set(true);
+        filterFormBatters.reset({
+            'search': '',
+            'filterColumn': 'all',
+            'flagged': false,
+            'opsPlusPotentialHasPosition': false,
+            'majorLeagueOnly': false
         });
-        this.sendGetBatterReport();
-        this.sendGetPitcherReport();
-      }),
-      catchError((error) => {
-        this.loadingSignal.set(false);
-        this.snackBar.open(`Error: ${error}`, 'Dismiss');
-        return of(error);
-      })
-    ).subscribe();
-  }
+        filterFormPitchers.reset({
+            'search': '',
+            'filterColumn': 'all',
+            'flagged': false,
+            'majorLeagueOnly': false
+        });
 
-  getBatterReport$ = this.getBatterReportAction.pipe(
-    switchMap(() => {
-      this.loadingSignal.set(true);
+        this.http.post('http://localhost:8080/runNotebook', {}, { responseType: 'text' }).pipe(
+            tap((res: any) => {
+                console.log(res);
+                this.loadingSignal.set(false);
+                this.snackBar.open(res, 'Dismiss', {
+                    duration: 7500
+                });
+                this.sendGetBatterReport();
+                this.sendGetPitcherReport();
+            }),
+            catchError((error) => {
+                console.log(error);
+                this.loadingSignal.set(false);
+                this.snackBar.open(`Error: ${error}`, 'Dismiss');
+                return of(error);
+            })
+        ).subscribe();
+    }
 
-      return this.http.get('http://localhost:5000/getBatterReport', {responseType: "text"}).pipe(
-        tap(res => {
-          this.loadingSignal.set(false);
-        }),
-        catchError((error) => {
-          this.loadingSignal.set(false);
-          console.log(error);
-          if(error.status != 404) {
-            this.snackBar.open(`Error: ${error.status}`, 'Dismiss');
-          }
-          return of(error);
+    getBatterReport$ = this.getBatterReportAction.pipe(
+        switchMap(() => {
+            this.loadingSignal.set(true);
+
+            return this.http.get('http://localhost:8080/getBatterReport', {responseType: "text"}).pipe(
+                tap(res => {
+                    this.loadingSignal.set(false);
+                }),
+                catchError((error) => {
+                    this.loadingSignal.set(false);
+                    console.log(error);
+                    if (error.status != 404) {
+                        this.snackBar.open(`Error: ${error.status}`, 'Dismiss');
+                    }
+                    return of(error);
+                })
+            );
         })
-      );
-    })
-  );
+    );
 
-  getPitcherReport$ = this.getPitcherReportAction.pipe(
-    switchMap(() => {
-      this.loadingSignal.set(true);
+    getPitcherReport$ = this.getPitcherReportAction.pipe(
+        switchMap(() => {
+            this.loadingSignal.set(true);
 
-      return this.http.get('http://localhost:5000/getPitcherReport', {responseType: "text"}).pipe(
-        tap(res => {
-          this.loadingSignal.set(false);
-        }),
-        catchError((error) => {
-          this.loadingSignal.set(false);
-          if(error.status != 404) {
-            this.snackBar.open(`Error: ${error.status}`, 'Dismiss');
-          }
-          return of(error);
+            return this.http.get('http://localhost:8080/getPitcherReport', {responseType: "text"}).pipe(
+                tap(res => {
+                    this.loadingSignal.set(false);
+                }),
+                catchError((error) => {
+                    this.loadingSignal.set(false);
+                    if (error.status != 404) {
+                        this.snackBar.open(`Error: ${error.status}`, 'Dismiss');
+                    }
+                    return of(error);
+                })
+            )
         })
-      )
-    })
-  );
+    );
 
-  getBatterReportSignal: Signal<any> = toSignal(this.getBatterReport$, {initialValue: null});
-  getPitcherReportSignal: Signal<any> = toSignal(this.getPitcherReport$, {initialValue: null});
+    getBatterReportSignal: Signal<any> = toSignal(this.getBatterReport$, {initialValue: null});
+    getPitcherReportSignal: Signal<any> = toSignal(this.getPitcherReport$, {initialValue: null});
 
-  sendGetBatterReport() {
-    this.getBatterReportSubject.next();
-  }
+    sendGetBatterReport() {
+        this.getBatterReportSubject.next();
+    }
 
-  sendGetPitcherReport() {
-    this.getPitcherReportSubject.next();
-  }
+    sendGetPitcherReport() {
+        this.getPitcherReportSubject.next();
+    }
 }
