52dc837f9437018bef9c0a27eace727316d15e01
nop990 <62253708+nop990@users.noreply.github.com>
2025-03-29 02:17:00 -0400

    formatted

--- src/app/dialogs/displayed-columns/displayed-columns.component.ts
+++ src/app/dialogs/displayed-columns/displayed-columns.component.ts
@@ -1,84 +1,86 @@
 import {ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, inject, OnInit} from '@angular/core';
 import {
-  columnMapBatters,
-  columnMapPitchers,
-  displayedColumnsBatters,
-  displayedColumnsBattersForm,
-  displayedColumnsPitchers, displayedColumnsPitchersForm
+    columnMapBatters,
+    columnMapPitchers,
+    displayedColumnsBatters,
+    displayedColumnsBattersForm,
+    displayedColumnsPitchers, displayedColumnsPitchersForm
 } from '../../utils';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 import {DisplayedColumnsService} from './displayed-columns.service';
 import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 
 @Component({
-  selector: 'app-displayed-columns',
-  standalone: false,
-  templateUrl: './displayed-columns.component.html',
-  styleUrl: './displayed-columns.component.scss'
+    selector: 'app-displayed-columns',
+    standalone: false,
+    templateUrl: './displayed-columns.component.html',
+    styleUrl: './displayed-columns.component.scss'
 })
 export class DisplayedColumnsComponent implements OnInit {
-  private type = inject(MAT_DIALOG_DATA);
-  private displayedColumnsService = inject(DisplayedColumnsService);
-  private dialogRef = inject(MatDialogRef<DisplayedColumnsComponent>);
-  private destroyRef = inject(DestroyRef);
+    private type = inject(MAT_DIALOG_DATA);
+    private displayedColumnsService = inject(DisplayedColumnsService);
+    private dialogRef = inject(MatDialogRef<DisplayedColumnsComponent>);
+    private destroyRef = inject(DestroyRef);
+    private endpoint = '';
 
-  protected columnMap;
-  protected displayedColumns;
-  protected displayedColumnsForm;
-  protected displayedColumnsSignal = this.displayedColumnsService.getDisplayedColumnsSignal;
+    protected columnMap;
+    protected displayedColumns;
+    protected displayedColumnsForm;
+    protected displayedColumnsSignal = this.displayedColumnsService.getDisplayedColumnsSignal;
 
-  ngOnInit() {
-  }
+    ngOnInit() {
 
-  constructor() {
-    if(this.type === 'batters') {
-      this.columnMap = columnMapBatters;
-      this.displayedColumns = displayedColumnsBatters;
-      this.displayedColumnsForm = displayedColumnsBattersForm;
-    } else {
-      this.columnMap = columnMapPitchers;
-      this.displayedColumns = displayedColumnsPitchers;
-      this.displayedColumnsForm = displayedColumnsPitchersForm;
     }
-  }
 
-  clearColumns() {
-    Object.keys(this.displayedColumnsForm.controls).forEach((key: string) => {
-      if (key !== 'name') {
-        // @ts-ignore
-        this.displayedColumnsForm.controls[key].setValue(false);
-      }
-    });
-  }
+    constructor() {
+        if (this.type === 'batters') {
+            this.columnMap = columnMapBatters;
+            this.displayedColumns = displayedColumnsBatters;
+            this.displayedColumnsForm = displayedColumnsBattersForm;
+        } else {
+            this.columnMap = columnMapPitchers;
+            this.displayedColumns = displayedColumnsPitchers;
+            this.displayedColumnsForm = displayedColumnsPitchersForm;
+        }
+    }
 
-  enableAllColumns() {
-    Object.keys(this.displayedColumnsForm.controls).forEach((key: string) => {
-      // @ts-ignore
-      this.displayedColumnsForm.controls[key].setValue(true);
-    });
-  }
+    clearColumns() {
+        Object.keys(this.displayedColumnsForm.controls).forEach((key: string) => {
+            if (key !== 'name') {
+                // @ts-ignore
+                this.displayedColumnsForm.controls[key].setValue(false);
+            }
+        });
+    }
 
-  saveColumns() {
-    let columns = '';
-    Object.keys(this.displayedColumnsForm.controls).filter(key => {
-      // @ts-ignore
-      if(this.displayedColumnsForm.controls[key].value) {
-        columns = columns + ',' + key;
-      }
-    });
-    columns = columns.slice(1, columns.length);
-    this.displayedColumnsService.setColumns(columns, this.type).pipe(takeUntilDestroyed(this.destroyRef)).subscribe();
-  }
+    enableAllColumns() {
+        Object.keys(this.displayedColumnsForm.controls).forEach((key: string) => {
+            // @ts-ignore
+            this.displayedColumnsForm.controls[key].setValue(true);
+        });
+    }
 
-  loadColumns() {
-    this.displayedColumnsService.sendGetDisplayedColumns(this.type);
-  }
+    saveColumns() {
+        let columns = '';
+        Object.keys(this.displayedColumnsForm.controls).filter(key => {
+            // @ts-ignore
+            if (this.displayedColumnsForm.controls[key].value) {
+                columns = columns + ',' + key;
+            }
+        });
+        columns = columns.slice(1, columns.length);
+        this.displayedColumnsService.setColumns(columns, this.type).pipe(takeUntilDestroyed(this.destroyRef)).subscribe();
+    }
 
-  close() {
-    this.dialogRef.close();
-  }
+    loadColumns() {
+        this.displayedColumnsService.sendGetDisplayedColumns(this.type)
+    }
 
-  stopSortingMyKeyValuesPlease() {
-    return 0;
-  }
+    close() {
+        this.dialogRef.close();
+    }
+
+    stopSortingMyKeyValuesPlease() {
+        return 0;
+    }
 }
--- src/app/dialogs/displayed-columns/displayed-columns.service.ts
+++ src/app/dialogs/displayed-columns/displayed-columns.service.ts
@@ -1,99 +1,101 @@
-import {inject, Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {computed, inject, Injectable, Signal, signal} from '@angular/core';
+import {HttpClient, httpResource} from '@angular/common/http';
 import {MatSnackBar} from '@angular/material/snack-bar';
 import {BehaviorSubject, catchError, of, switchMap, tap} from 'rxjs';
 import {toSignal} from '@angular/core/rxjs-interop';
 import {displayedColumnsBattersForm, displayedColumnsPitchersForm} from '../../utils';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class DisplayedColumnsService {
-  private http = inject(HttpClient);
-  private snackbar = inject(MatSnackBar);
+    private http = inject(HttpClient);
+    private snackbar = inject(MatSnackBar);
 
-  private GetDisplayedColumnsSubject = new BehaviorSubject<string>('');
-  GetDisplayedColumnsAction = this.GetDisplayedColumnsSubject.asObservable();
+    private GetDisplayedColumnsSubject = new BehaviorSubject<string>('');
+    GetDisplayedColumnsAction = this.GetDisplayedColumnsSubject.asObservable();
 
-  public sendGetDisplayedColumns(type: string) {
-    this.GetDisplayedColumnsSubject.next(type);
-  }
-
-  getDisplayedColumns$ = this.GetDisplayedColumnsAction.pipe(
-    switchMap((type) => {
-      let endpoint;
-      if (type === 'batters') {
-        endpoint = 'getBatterColumns';
-      } else if (type === 'pitchers') {
-        endpoint = 'getPitcherColumns';
-      } else {
-        return of();
-      }
-      return this.http.get('http://localhost:8080/' + endpoint, {responseType: "text"}).pipe(
-        tap(res => {
-          this.detectColumns(res, type);
-          this.snackbar.open('Columns loaded', 'Dismiss', {
-            duration: 7500
-          });
-        }),
-        catchError((error) => {
-          console.log(error);
-          if (error.status != 404) {
-            this.snackbar.open(`Error: ${error.status}`, 'Dismiss');
-          }
-          return of(error);
-        })
-      );
-    }));
-
-  getDisplayedColumnsSignal = toSignal(this.getDisplayedColumns$);
-
-  detectColumns(columnsTxt: string, type: string) {
-    let columns = columnsTxt.split(',');
-    let form;
-
-    if (type === 'batters') {
-      form = displayedColumnsBattersForm;
-    } else {
-      form = displayedColumnsPitchersForm;
+    public sendGetDisplayedColumns(type: string) {
+        this.GetDisplayedColumnsSubject.next(type);
     }
 
-    Object.keys(form.controls).forEach((key: string) => {
-      // @ts-ignore
-      form.controls[key].setValue(false);
-    })
+    getDisplayedColumns$ = this.GetDisplayedColumnsAction.pipe(
+        switchMap((type) => {
+            let endpoint;
+            if (type === 'batters') {
+                endpoint = 'getBatterColumns';
+            } else if (type === 'pitchers') {
+                endpoint = 'getPitcherColumns';
+            } else {
+                return of();
+            }
+            return this.http.get('http://localhost:8080/' + endpoint, {responseType: "text"}).pipe(
+                tap(res => {
+                    this.detectColumns(res, type);
+                    this.snackbar.open('Columns loaded', 'Dismiss', {
+                        duration: 7500
+                    });
+                }),
+                catchError((error) => {
+                    console.log(error);
+                    if (error.status != 404) {
+                        this.snackbar.open(`Error: ${error.status}`, 'Dismiss');
+                    }
+                    return of(error);
+                })
+            );
+        }));
 
-    for (let column of columns) {
-      Object.keys(form.controls).forEach((key: string) => {
-        if (key === column) {
-          // @ts-ignore
-          form.controls[key].setValue(true);
+    getDisplayedColumnsSignal = toSignal(this.getDisplayedColumns$);
+
+    detectColumns(columnsTxt: string, type: string) {
+        let columns = columnsTxt.split(',');
+        let form;
+
+        if (type === 'batters') {
+            form = displayedColumnsBattersForm;
+        } else {
+            form = displayedColumnsPitchersForm;
         }
-      });
+
+        console.log(columns, form);
+
+        Object.keys(form.controls).forEach((key: string) => {
+            // @ts-ignore
+            form.controls[key].setValue(false);
+        })
+
+        for (let column of columns) {
+            Object.keys(form.controls).forEach((key: string) => {
+                if (key === column) {
+                    // @ts-ignore
+                    form.controls[key].setValue(true);
+                }
+            });
+        }
     }
-  }
 
-  setColumns(columns: any, type: string) {
-    let endpoint;
-    if (type === 'batters') {
-      endpoint = 'setBatterColumns';
-    } else if (type === 'pitchers') {
-      endpoint = 'setPitcherColumns';
+    setColumns(columns: any, type: string) {
+        let endpoint;
+        if (type === 'batters') {
+            endpoint = 'setBatterColumns';
+        } else if (type === 'pitchers') {
+            endpoint = 'setPitcherColumns';
+        }
+
+        console.log(columns);
+
+        return this.http.post('http://localhost:8080/' + endpoint, columns, {responseType: 'text'}).pipe(
+            tap((res: any) => {
+                console.log(res);
+                this.snackbar.open(res, 'Dismiss', {
+                    duration: 7500
+                });
+            }),
+            catchError((error) => {
+                this.snackbar.open(`Error: ${error}`, 'Dismiss');
+                return of(error);
+            })
+        );
     }
-
-    console.log(columns);
-
-    return this.http.post('http://localhost:8080/' + endpoint, columns).pipe(
-      tap((res: any) => {
-        console.log(res);
-        this.snackbar.open(res, 'Dismiss', {
-          duration: 7500
-        });
-      }),
-      catchError((error) => {
-        this.snackbar.open(`Error: ${error}`, 'Dismiss');
-        return of(error);
-      })
-    );
-  }
 }
