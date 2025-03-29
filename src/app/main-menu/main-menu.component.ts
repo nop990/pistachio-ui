c49907a63f0a2f65052ed4e2dbcc398c78b9724d
nop990 <62253708+nop990@users.noreply.github.com>
2025-03-29 02:25:46 -0400

    Update main-menu.component.ts

--- src/app/main-menu/main-menu.component.ts
+++ src/app/main-menu/main-menu.component.ts
@@ -1,7 +1,6 @@
 import {Component, computed, inject, OnInit, Signal} from '@angular/core';
 import {PistachioService} from '../pistachio.service';
 import {MatTableDataSource} from '@angular/material/table';
-import {DataManipulationService} from '../data-manipulation.service';
 import {of} from 'rxjs';
 import {BatterReport} from "../models/batter-report.model";
 import {PitcherReport} from "../models/pitcher-report.model";
